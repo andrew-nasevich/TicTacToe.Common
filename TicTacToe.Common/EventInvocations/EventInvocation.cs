1408dd25ead6bc5be3a5ddfac98b73dfad4ab2ac
Andrew <nasevichandrey@gmail.com>
2019-12-05 10:42:44 +0300

    EventInvoker was implemented

---
+++ TicTacToe.Common/EventInvocations/EventInvocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TicTacToe.Common.Interfaces;
+
+namespace TicTacToe.Common.EventInvocations
+{
+    public class EventInvoker : IEventInvoker
+    {
+        public void Invoke<TEventArgs>(EventHandler<TEventArgs> eventHandler, object sender, TEventArgs e)
+            where TEventArgs : EventArgs
+        {
+            eventHandler?.Invoke(sender, e);
+        }
+    }
+}
---
+++ TicTacToe.Common/Interfaces/IEventInvoker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TicTacToe.Common.Interfaces
+{
+    public interface IEventInvoker
+    { 
+        void Invoke<TEventArgs>(EventHandler<TEventArgs> eventHandler, object sender, TEventArgs e)
+            where TEventArgs : EventArgs;
+    }
+}
