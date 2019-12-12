f6376ea2236bf0e66c9640a632052aa4c525a3c0
Andrew <nasevichandrey@gmail.com>
2019-12-12 10:44:33 +0300

    EnumerableExtensions were implemented

---
+++ TicTacToe.Common/EnumerableExtensions/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToe.Common.EnumerableExtensions
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> collection, int index, Action<int, T> action)
+        {
+            foreach (var el in collection)
+            {
+                action(index++, el);
+            }
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var el in collection)
+            {
+                action(el);
+            }
+        }
+    }
+}
